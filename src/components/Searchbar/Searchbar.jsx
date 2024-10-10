e9d4a32db65331de1409dd302bcbc78c67487605
AlexandraPlaton16 <platonmariaalexandra@yahoo.com>
2024-10-10 23:26:42 +0200

    commit

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { FaSearch } from 'react-icons/fa'; // Ensure this icon is used
+import styles from './Searchbar.module.css';
+
+class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = e => {
+    this.setState({ query: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.query);
+  };
+
+  render() {
+    return (
+      <header className={styles.Searchbar}>
+        <form className={styles.SearchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={styles.SearchFormButton}>
+            <FaSearch style={{ color: '#fff', fontSize: '20px' }} />{' '}
+            {/* Using FaSearch here */}
+          </button>
+
+          <input
+            className={styles.SearchFormInput}
+            type="text"
+            value={this.state.query}
+            onChange={this.handleChange}
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
